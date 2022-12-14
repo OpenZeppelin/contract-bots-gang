1c34cfb675e0a431f92ab353a5f778b31d54ced6
Josep Chetrit <jchetrit1999@gmail.com>
2022-12-14 19:00:32 +0100

    Add logic to detect new smart contracts created with CREATE/CREATE2 opcode

--- contract-deconstruct/src/agent.ts
+++ contract-deconstruct/src/agent.ts
@@ -6,12 +6,13 @@ import {
   TransactionEvent,
   getEthersProvider,
 } from "forta-agent";
-import {disassemble} from '@ethersproject/asm';
+import { disassemble } from "@ethersproject/asm";
 import * as dotenv from "dotenv";
-import * as importedFunctions from "./static/functions.json"
-import * as importedEvents from "./static/events.json"
-import { deflate } from 'node:zlib'
-import { promisify } from 'node:util'
+import * as importedFunctions from "./static/functions.json";
+import * as importedEvents from "./static/events.json";
+import { deflate } from "node:zlib";
+import { promisify } from "node:util";
+import { getCreatedContractsTX } from "./utils";
 
 dotenv.config();
 
@@ -19,36 +20,46 @@ dotenv.config();
 const asyncDeflate = promisify(deflate);
 var ethProvider: ethers.providers.JsonRpcProvider;
 
-const disassembleBytecode = async(contractAddress: string) => {
+const disassembleBytecode = async (contractAddress: string) => {
   var deployedBytecode;
   try {
     deployedBytecode = await ethProvider.getCode(contractAddress);
-  } catch(error) {
+  } catch (error) {
     console.info("Public provider failed answering to 'getCode', exiting ");
     return;
   }
-  
-  if(!deployedBytecode) return null;
+
+  if (!deployedBytecode) return null;
 
   //const analysis = yasold.analyze(deployedBytecode)
-  var analysis = ''
+  var analysis = "";
 
   var disassembled = disassemble(deployedBytecode);
   var byte4DirectoryFunctions: string[] = [];
   var byte4DirectoryEvents: string[] = [];
-  var mapping: Map<string,boolean> = new Map();
+  var mapping: Map<string, boolean> = new Map();
 
-  for(var i = 0; i<disassembled.length; i++) {
+  for (var i = 0; i < disassembled.length; i++) {
     var mnemonic = disassembled[i].opcode.mnemonic;
-    if((mnemonic == 'PUSH4' || mnemonic == 'PUSH32' ) && !mapping.has(disassembled[i].pushValue as string)) {
+    if (
+      (mnemonic == "PUSH4" || mnemonic == "PUSH32") &&
+      !mapping.has(disassembled[i].pushValue as string)
+    ) {
       mapping.set(disassembled[i].pushValue as string, true);
-      if(mnemonic == 'PUSH4') byte4DirectoryFunctions.push(disassembled[i].pushValue as string);
+      if (mnemonic == "PUSH4")
+        byte4DirectoryFunctions.push(disassembled[i].pushValue as string);
       else byte4DirectoryEvents.push(disassembled[i].pushValue as string);
     }
   }
 
-  return {byte4DirectoryFunctions,byte4DirectoryEvents, disassembled, analysis, deployedBytecode}
-}
+  return {
+    byte4DirectoryFunctions,
+    byte4DirectoryEvents,
+    disassembled,
+    analysis,
+    deployedBytecode,
+  };
+};
 
 const getFunctions = async (byte4DirectoryFunctions: string[]) => {
   var newFunction: object;
@@ -56,24 +67,24 @@ const getFunctions = async (byte4DirectoryFunctions: string[]) => {
   var unknownFunctions: string[] = [];
   var functionSignatures: any = importedFunctions;
 
-  for(var j = 0; j<byte4DirectoryFunctions.length; j++) {
+  for (var j = 0; j < byte4DirectoryFunctions.length; j++) {
     var results = functionSignatures[`${byte4DirectoryFunctions[j]}`];
-    if(results && results.length != 0) {
-      for(var k = 0; k<results.length; k++){
+    if (results && results.length != 0) {
+      for (var k = 0; k < results.length; k++) {
         newFunction = {
-          "text": results[k],
-          "hex": byte4DirectoryFunctions[j]
-        }
+          text: results[k],
+          hex: byte4DirectoryFunctions[j],
+        };
 
         functions.push(newFunction);
       }
     } else {
-      unknownFunctions.push(byte4DirectoryFunctions[j])
+      unknownFunctions.push(byte4DirectoryFunctions[j]);
     }
   }
 
-  return {functions, unknownFunctions};
-}
+  return { functions, unknownFunctions };
+};
 
 const getEvents = async (byte4DirectoryEvents: string[]) => {
   var newEvent: object;
@@ -81,94 +92,103 @@ const getEvents = async (byte4DirectoryEvents: string[]) => {
   var unkownEvents: string[] = [];
   var eventSignatures: any = importedEvents;
 
-  for(var l = 0; l<byte4DirectoryEvents.length; l++) {
+  for (var l = 0; l < byte4DirectoryEvents.length; l++) {
     var results = eventSignatures[`${byte4DirectoryEvents[l]}`];
-    if(results && results.length != 0) {
-      for(var m = 0; m<results.length; m++){
+    if (results && results.length != 0) {
+      for (var m = 0; m < results.length; m++) {
         newEvent = {
-          "text": results[m],
-          "hex": byte4DirectoryEvents[l]
-        }
+          text: results[m],
+          hex: byte4DirectoryEvents[l],
+        };
         events.push(newEvent);
       }
     } else {
-      unkownEvents.push(byte4DirectoryEvents[l])
+      unkownEvents.push(byte4DirectoryEvents[l]);
     }
   }
 
-  return {events, unkownEvents};
-}
+  return { events, unkownEvents };
+};
 
-const handleTransactionInternal = async (
-  contractAddress: string
-) => {  
+const handleTransactionInternal = async (contractAddress: string) => {
   var getDisassembleResult: any = await disassembleBytecode(contractAddress);
 
-  var getFunctionsResult = await getFunctions(getDisassembleResult.byte4DirectoryFunctions);
+  var getFunctionsResult = await getFunctions(
+    getDisassembleResult.byte4DirectoryFunctions
+  );
 
-  var getEventsResult = await getEvents(getDisassembleResult.byte4DirectoryEvents);
+  var getEventsResult = await getEvents(
+    getDisassembleResult.byte4DirectoryEvents
+  );
 
   return {
-    functions: getFunctionsResult.functions, 
-    unknownFunctions: getFunctionsResult.unknownFunctions, 
-    events: getEventsResult.events, 
-    unkownEvents: getEventsResult.unkownEvents, 
-    disassembled: getDisassembleResult.disassembled, 
+    functions: getFunctionsResult.functions,
+    unknownFunctions: getFunctionsResult.unknownFunctions,
+    events: getEventsResult.events,
+    unkownEvents: getEventsResult.unkownEvents,
+    disassembled: getDisassembleResult.disassembled,
     //analysis: getDisassembleResult.analysis,
-    bytecode: getDisassembleResult.deployedBytecode
+    bytecode: getDisassembleResult.deployedBytecode,
   };
 };
 
-const runTx = async (txEvent: TransactionEvent) => {
+const runTx = async (
+  contracts: {
+    deployer: string;
+    contractAddress: string;
+    transaction: string;
+  }[]
+) => {
   let findings: Finding[] = [];
 
-  let transaction = txEvent.transaction;
-  if(transaction.to == null || transaction.to == '0x0000000000000000000000000000000000000000') {
-    if(!ethProvider) ethProvider = getEthersProvider();
-    const receipt = await ethProvider.getTransactionReceipt(transaction.hash);
-    if(!receipt) return;
-    const contractAddressFromReceipt = receipt.contractAddress;
-    if(receipt.status && contractAddressFromReceipt) {
-      const result = await handleTransactionInternal(contractAddressFromReceipt)
-      var compressedDisassembled: any = await asyncDeflate(JSON.stringify(result.disassembled));
-      compressedDisassembled = compressedDisassembled.toString('base64');
+  if (contracts && contracts.length) {
+    for (let i = 0; i < contracts.length; i++) {
+      if(!ethProvider) ethProvider = getEthersProvider();
+      const result = await handleTransactionInternal(
+        contracts[i].contractAddress
+      );
+      var compressedDisassembled: any = await asyncDeflate(
+        JSON.stringify(result.disassembled)
+      );
+      compressedDisassembled = compressedDisassembled.toString("base64");
 
       // var compressedAnalysis: any = await asyncDeflate(JSON.stringify(result.analysis))
       // compressedAnalysis = compressedAnalysis.toString('base64');
-      
+
       findings.push(
-          Finding.fromObject({
-            name: `CD-${new Date().getTime()}`,
-            description: `Contract deconstruct ${contractAddressFromReceipt.substring(0,10)}`,
-            alertId: `CD-${new Date().getTime()}`,
-            severity: FindingSeverity.Info,
-            type: FindingType.Info,
-            metadata: {
-              transaction: transaction.hash,
-              contractAddress: contractAddressFromReceipt,
-              functions: JSON.stringify(result.functions),
-              unknownFunctions: JSON.stringify(result.unknownFunctions),
-              events: JSON.stringify(result.events),
-              unknownEvents: JSON.stringify(result.unkownEvents),
-              bytecode: JSON.stringify(result.bytecode),
-              disassembled: compressedDisassembled,
-              //analysis: compressedAnalysis
-            },
-          })
-        );
+        Finding.fromObject({
+          name: `CD-${new Date().getTime()}`,
+          description: `Contract deconstruct ${contracts[i].contractAddress.substring(0, 10)}`,
+          alertId: `CD-${new Date().getTime()}`,
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: {
+            transaction: contracts[i].transaction,
+            contractAddress: contracts[i].contractAddress,
+            functions: JSON.stringify(result.functions),
+            unknownFunctions: JSON.stringify(result.unknownFunctions),
+            events: JSON.stringify(result.events),
+            unknownEvents: JSON.stringify(result.unkownEvents),
+            bytecode: JSON.stringify(result.bytecode),
+            disassembled: compressedDisassembled,
+            //analysis: compressedAnalysis
+          },
+        })
+      );
     }
-  };
+  }
 
   // if transaction contains CREATE2 execution OPCODE, take out address and take bytecode
   return findings;
-}
+};
 
 const handleTransaction = async (txEvent: TransactionEvent) => {
-  return await runTx(txEvent);
-}
+  const contracts = await getCreatedContractsTX(txEvent);
+  if (contracts && contracts.length) {
+    return await runTx(contracts);
+  }
+};
 
 export default {
   handleTransaction,
 };
-
-
---
+++ contract-deconstruct/src/utils.ts
@@ -0,0 +1,46 @@
+import { TransactionEvent, ethers, getEthersProvider } from "forta-agent";
+
+var ethProvider: ethers.providers.JsonRpcProvider;
+
+export async function getCreatedContractsTX(txEvent: TransactionEvent){
+
+    const contracts: {
+        deployer: string,
+        contractAddress: string,
+        transaction: string,
+    }[] = [];
+
+    let transaction = txEvent.transaction;
+
+    if(transaction.to === null || transaction.to === "0x0000000000000000000000000000000000000000"){
+        if(!ethProvider) ethProvider = getEthersProvider();
+        const receipt = await ethProvider.getTransactionReceipt(transaction.hash);
+        if(!receipt) return;
+        const contractAddressFromReceipt = receipt.contractAddress;
+        contracts.push({
+            deployer: transaction.from,
+            contractAddress: contractAddressFromReceipt,
+            transaction: transaction.hash,
+        })
+    } else {
+        if(!ethProvider) ethProvider = getEthersProvider();
+        const bytecode = await ethProvider.getCode(transaction.to);
+        if(bytecode.length > 0){
+            for (const trace of txEvent.traces) {
+                if(trace.type === "create"){
+                    if (trace.result.address) {
+                        contracts.push({
+                          deployer: trace.action.from,
+                          contractAddress: trace.result.address,
+                          transaction: transaction.hash
+                        });
+                        continue;
+                      }
+                }
+            }
+        }
+    }
+
+    return contracts;
+
+}
